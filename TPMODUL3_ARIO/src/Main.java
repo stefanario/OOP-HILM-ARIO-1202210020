2c360ce6e8fabe2a97ca87e25fc2bb5a59e9db03
stefanario <116353759+stefanario@users.noreply.github.com>
2022-12-25 18:24:17 +0700

    Add files via upload

---
+++ TPMODUL3_ARIO/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Restaurant restaurant = new Restaurant();
+        int customerID, orderQty;
+        try {
+
+            System.out.println("Enter Customer ID: ");
+            customerID = input.nextInt();
+
+            System.out.println("Enter how much food to made: ");
+            orderQty = input.nextInt();
+
+            Thread t1 = new Thread(restaurant);
+            Waiters waiters = new Waiters(customerID, orderQty);
+            Thread t2 = new Thread(waiters);
+
+            t1.start();
+            t2.start();
+            t1.join();
+            t2.join();
+
+        } catch (Exception e) {
+            System.out.println("Input must be Integer");
+        }
+
+    }
+
+}
